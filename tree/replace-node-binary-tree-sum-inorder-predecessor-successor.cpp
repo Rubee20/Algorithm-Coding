20e3929e77c75984271eccef595ee7a956675bdd
Rubee <rubee20tanden@gmail.com>
2019-12-10 21:41:47 +0530

    Binary Tree

--- tree/replace-node-binary-tree-sum-inorder-predecessor-successor.cpp
+++ tree/replace-node-binary-tree-sum-inorder-predecessor-successor.cpp
@@ -76,7 +76,7 @@ int main()
 	fun1(root);
 	//cout << "2 " << endl;
 
-	cout << endl <<"after sum inorder predecessor successor" << endl;
+	cout << endl <<"after replacing node data sum inorder predecessor successor" << endl;
 	inorder(root);
 
 	return 0;
