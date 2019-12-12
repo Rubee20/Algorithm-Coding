8fa8d4d2e7d1692a95fbf496cbc5a10c48987bd2
Rubee <rubee20tanden@gmail.com>
2019-12-12 20:57:09 +0530

    find_max_difference_bwt_2_nu.cpp

--- find_max_difference_bwt_2_nu.cpp
+++ find_max_difference_bwt_2_nu.cpp
@@ -10,18 +10,14 @@ int find_max_difference_bwt_max_min(int array[], int size)
 	int i;
 	for (i = size-1; i>=0; i--)
 	{
-		//cout << "Rubee ";
 		if (max_number < array[i])
 		{
-			cout << "Rubee " << endl;
 			max_number = array[i];
 
-			//out << "Rubee " << max_number << endl;
 		}
 
 		if (max_difference < max_number - array[i])
 		{
-			cout << "Rubee 1 " << max_difference << endl;
 			max_difference = max_number - array[i];
 		}
 	}
@@ -31,7 +27,6 @@ int find_max_difference_bwt_max_min(int array[], int size)
 int main()
 {
 	int array[] = {2, 7, 1, 5, 9, 3, 5};
-	//int array[] = {9, 1 };
 
 	int size = sizeof(array)/sizeof(array[0]);
 
